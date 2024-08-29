7531481c74a3c8bdf5c7527e648a899d6ddeea0a
Robin <robin@metropolia.fi>
2024-08-30 01:04:44 +0530

    Module 3

---
+++ Module 3(Part3) Conditional structures (if).py
@@ -0,0 +1,22 @@
+gender = input('Enter your biological gender (male/female)').lower()
+hemoglobin_value = float(input('Enter your hemoglobin value (g/l):'))
+if gender == 'female':
+    if hemoglobin_value < 117:
+       print('Hemoglobin value is low.')
+    elif 117 <= hemoglobin_value <=155:
+        print('Hemoglobin value is normal.')
+    else:
+        print('Hemoglobin value is high.')
+if gender == 'male':
+    if hemoglobin_value < 134:
+        print('Hemoglobin value is low.')
+    elif 134 <= hemoglobin_value <= 167:
+        print('Hemoglobin value is normal.')
+    else:
+        print('Hemoglobin value is high.')
+else:
+    print('Invalid gender input')
+
+
+
+
