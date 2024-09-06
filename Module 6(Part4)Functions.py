b79fb6fa5337915e317f1dc6394f6983c8262033
Robin <robin@metropolia.fi>
2024-09-07 00:40:56 +0530

    Module 6

---
+++ Module 6(Part4)Functions.py
@@ -0,0 +1,10 @@
+import random
+
+test_list = []
+for number in range(1,10):
+    test_list.append(random.randint (1,10))
+print(f'The list of the number is: ')
+for i in range (len(test_list)):
+    print(test_list[i], end= ' ')
+print(f'\nThe sum of all items in the list is: {sum(test_list)}')
+
