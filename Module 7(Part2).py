5bb9d7ce567b28e8d7738dd53f0bba661bdcef8d
Robin <robin@metropolia.fi>
2024-09-09 22:48:00 +0530

    Module 7

---
+++ Module 7(Part2).py
@@ -0,0 +1,19 @@
+names_set = set()
+
+while True:
+
+    name = input("Enter a name (or press Enter to finish): ")
+
+    if name == "":
+        break
+    if name in names_set:
+        print("Existing name")
+    else:
+        print("New name")
+
+        names_set.add(name)
+
+print("\nList of entered names:")
+for name in names_set:
+    print(name)
+
