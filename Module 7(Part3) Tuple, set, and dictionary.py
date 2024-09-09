18e777892fce69d8f13e3281c9128cd60c3c7d06
Robin <robin@metropolia.fi>
2024-09-10 00:03:54 +0530

    Module 7

---
+++ Module 7(Part3) Tuple, set, and dictionary.py
@@ -0,0 +1,35 @@
+from random import choice
+
+airport_data = {}
+def add_airport():
+    icao_code = input('Enter the ICAO code of the airport: ')
+    airport_name = input('Enter airport name: ')
+
+    airport_data[icao_code] = airport_name
+    print(f'Airport {airport_name} with ICAO code {icao_code} added.\n')
+
+def fetch_airport():
+    icao_code = input('Enter the ICAO code of the airport: ').upper()
+    if icao_code in airport_data:
+        print(f'The airport with ICAO code {icao_code} is {airport_data[icao_code]}.\n')
+    else:
+        print(f'No airport found with ICAO code {icao_code}.\n')
+def main():
+    while True:
+        print('Choose an option: ')
+        print("1. Enter a new airport")
+        print("2. Fetch airport information")
+        print("3. Quit")
+
+        choice = input('Enter  your choce (1/2/3): ')
+        if choice == '1':
+            add_airport()
+        elif choice == '2':
+            fetch_airport()
+        elif choice == '3':
+            print('Exiting the program.')
+            break
+        else:
+            print('Invalid choice. Please enter 1,2,3.\n')
+
+main()
